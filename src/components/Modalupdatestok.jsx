d03cc9d41dce17da2e6714229cad2768bc161618
AntioMood <brigita079@gmail.com>
2026-02-22 19:12:13 +0700

    up

--- src/components/Modalupdatestok.jsx
+++ src/components/Modalupdatestok.jsx
@@ -36,7 +36,7 @@ const ModalUpdateStok = ({ isOpen, onClose, product, onSuccess }) => {
 
         try {
             await axios.post(
-                `https://bangunan.fremwe.my.id/api/produk/${id_produk}`,
+                `https://bangunan.fremwe.my.id/api/produk/${item.id_produk}`,
                 {
                     jumlah: jumlah, // bisa positif atau negatif
                 },
